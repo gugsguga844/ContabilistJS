169e9630243fab35ed8fe033a975004170e493f5
Gustavo Schneider <gusta.schneider123@gmail.com>
2023-12-10 23:47:32 -0300

    eslint quick fix

--- app-pages/sidebar.js
+++ app-pages/sidebar.js
@@ -1,14 +1,15 @@
+/* eslint-disable no-undef */
 $(function() {
-    $('.buttonMenu').click(function() {
-        const containerSidebar = $('#container-sidebar');
-        const buttonIcon = $('.buttonMenu i');
+	$('.buttonMenu').click(function() {
+		const containerSidebar = $('#container-sidebar');
+		const buttonIcon = $('.buttonMenu i');
 
-        if (containerSidebar.hasClass('sidebar')) {
-        containerSidebar.removeClass('sidebar').addClass('sidebar-hidden');
-        } else {
-        containerSidebar.removeClass('sidebar-hidden').addClass('sidebar');
-        }
+		if (containerSidebar.hasClass('sidebar')) {
+			containerSidebar.removeClass('sidebar').addClass('sidebar-hidden');
+		} else {
+			containerSidebar.removeClass('sidebar-hidden').addClass('sidebar');
+		}
 
-        buttonIcon.toggleClass('fa-times');
-    });
+		buttonIcon.toggleClass('fa-times');
+	});
 });
